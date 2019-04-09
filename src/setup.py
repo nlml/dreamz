f37b1505ffd878ea0f052fa83c3a8fe235129eae
liam schoneveld <liam.sch@gmail.com>
2019-04-09 22:00:53 +0200

    make package

---
+++ src/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dreamz',
+      version='0.1',
+      description='deep learning-driven visual art stuff',
+      author='Liam Schoneveld',
+      author_email='liam.sch@gmail.com',
+      packages=['dreamz'])
